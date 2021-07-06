ae1448c58ed2130cf2390ec5581a8105f5e93155
uno <rubenlladoc@gmail.com>
2021-07-06 17:14:52 +0800

    added images

--- app/src/main/java/com/uno/diceroller/MainActivity.kt
+++ app/src/main/java/com/uno/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.uno.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -29,12 +30,20 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
 
         val diceRoll = dice.roll()
-        val diceRoll2 = dice.roll()
 
         // Update the screen with the dice roll
-        val resultTextView : TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-        val resultTextView2 : TextView = findViewById(R.id.textView2)
-        resultTextView2.text = diceRoll2.toString()
+        val diceImage : ImageView = findViewById(R.id.imageView)
+
+        val drawableResource = when(diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
